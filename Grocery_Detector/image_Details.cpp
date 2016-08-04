c0038dac6aaf95177537e70360e885b1d1774352
Wyatt Ogden <wyatt_ogden@yahoo.com>
2016-08-03 21:08:22 -0700

    Create image_Details.cpp

---
+++ Grocery_Detector/image_Details.cpp
@@ -0,0 +1,27 @@
+#include "stdafx.h"
+#include "image_Details.h"
+
+
+image_Details::image_Details()
+{
+}
+
+ostream& operator << (ostream& outfile, image_Details& img_info) {
+	outfile << img_info.filename << endl;
+	outfile << img_info.rowStart << endl;
+	outfile << img_info.rowEnd << endl;
+	outfile << img_info.similarity << endl;
+	outfile << "END OF IMG." << endl;
+	return outfile;
+}
+
+istream& operator >>(ifstream& infile, image_Details& img_info) {
+	infile >> img_info.rowStart;
+	infile >> img_info.rowEnd;
+	infile >> img_info.similarity;
+	return infile;
+}
+
+image_Details::~image_Details()
+{
+}
